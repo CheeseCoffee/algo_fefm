e1842e68431603a43b9f61e6c0da47692bbc4f30
akbar <akbarsky93@mail.ru>
2021-09-16 13:44:22 +0300

    dot counts

--- sem2/quarter.py
+++ sem2/quarter.py
@@ -1,10 +1,22 @@
-x = int(input())
-y = int(input())
-if x>0 and y>0:
-    print("first quarter")
-elif x>0 and y<0:
-    print("fourth quarter")
-elif y>0:
-    print("second quarter")
+N = int(input())
+first = second = third = fourth = 0
+for i in range(N):
+    x, y = str(input()).split()
+    x = int(x)
+    y = int(y)
+    if x > 0 and y > 0:
+        first += 1
+    elif x > 0 and y < 0:
+        fourth += 1
+    elif x < 0 and y > 0:
+        second += 1
+    elif x < 0 and y < 0:
+        third += 1
+if first >= second and first >= third and first >= fourth:
+    print(1, first)
+elif second >= third and second >= fourth:
+    print(2, second)
+elif third >= fourth:
+    print(3, third)
 else:
-    print("third quarter")
+    print(4, fourth)
